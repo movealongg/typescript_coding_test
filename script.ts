a7fea7ff77715ec29d380d6fb16fdfd3de70b77d
Aliyah <anorthington@ufl.edu>
2019-10-24 15:30:09 -0500

    Fix TS error
    Error reads: Property 'value' does not exist on type 'HTMLElement'

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 function palindrome(){
   //Store element from #user-input in input variable
-  let input: string = document.getElementById("user-input").value;
+  let input: string = (<HTMLInputElement> document.getElementById("user-input")).value;
   alert(input);
 }
