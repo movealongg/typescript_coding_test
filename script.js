45210a898cc02b985d77fb96fabb5fa567b18cf2
Aliyah <anorthington@ufl.edu>
2019-10-24 13:19:10 -0500

    Start ts file

---
+++ script.js
@@ -0,0 +1,5 @@
+function palindrome() {
+    //Store element from #user-input in input variable
+    var input = document.getElementById("user-input").value;
+    alert(input);
+}
---
+++ script.ts
@@ -0,0 +1,5 @@
+function palindrome(){
+  //Store element from #user-input in input variable
+  let input: string = document.getElementById("user-input").value;
+  alert(input);
+}
