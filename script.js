d462fc767bced015c2b9323d4ecc8d96c181c99b
Aliyah <anorthington@ufl.edu>
2019-10-24 16:10:22 -0500

    Use js methods to split, reverse, and join input
    This commit also compares the input to the
    reversed input to see if it is a palindrome.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function palindrome() {
     //Store element from #user-input in input variable
     var input = document.getElementById("user-input").value;
+    //Split input string into substrings using split method
     var splitInput = input.split("");
-    alert(splitInput);
+    //Reverse substrings using reverse method
+    var reverseInput = splitInput.reverse();
+    //Put substrings back together
+    var joinInput = reverseInput.join("");
+    if (input == joinInput) {
+        alert('This is a palidrome!');
+    }
+    else {
+        alert('This is not a palindrome!');
+    }
+    // alert(joinInput);
 }
--- script.ts
+++ script.ts
@@ -1,8 +1,17 @@
 function palindrome(){
   //Store element from #user-input in input variable
   let input: string = (<HTMLInputElement> document.getElementById("user-input")).value;
-  //Split input string into substrings using split method 
+  //Split input string into substrings using split method
   let splitInput: string[] = input.split("");
-  alert(splitInput);
+  //Reverse substrings using reverse method
+  let reverseInput: string[] = splitInput.reverse();
+  //Put substrings back together
+  let joinInput: string = reverseInput.join("");
+
+  if (input == joinInput){
+    alert('This is a palidrome!');
+  }else{
+    alert('This is not a palindrome!');
+  }
 
 }
