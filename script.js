3bc105b1cfc36f0120be1358ab1700f2be89db8f
Aliyah <anorthington@ufl.edu>
2019-10-25 10:25:15 -0500

    Update js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function palindrome() {
     //Store element from #user-input in input variable
-    var input = document.getElementById("user-input").value;
+    let input = document.getElementById("user-input").value;
     //Split input string into substrings using split method
-    var splitInput = input.split("");
+    let splitInput = input.split("");
     //Reverse substrings using reverse method
-    var reverseInput = splitInput.reverse();
+    let reverseInput = splitInput.reverse();
     //Put substrings back together
-    var joinInput = reverseInput.join("");
+    let joinInput = reverseInput.join("");
     //If input is empty or only has whitespace, ask to enter some characters
     if (input == "" || input.trim().length === 0) {
         document.getElementById("results").innerHTML = "Please enter some characters to check.";
